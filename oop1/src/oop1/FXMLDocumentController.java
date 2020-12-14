16b6fb43297c9c8c3425b686c9c3e602fba87203
Berkhan <berkhanygt97@gmail.com>
2020-12-14 19:29:25 +0300

    butona tıklayınca correctleme çalışmıyor şuan

--- oop1/src/oop1/FXMLDocumentController.java
+++ oop1/src/oop1/FXMLDocumentController.java
@@ -5,10 +5,22 @@
  */
 package oop1;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 /**
  * FXML Controller class
  *
@@ -16,9 +28,49 @@ import javafx.fxml.Initializable;
  */
 public class FXMLDocumentController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+   @FXML
+    private Button SpellCheck;
+   @FXML
+    private TextField SearchButton;
+   @FXML
+    private Button Open;
+   @FXML
+    private Button Save;
+   @FXML
+    public TextArea InputText;
+    @FXML
+    private Button Search;
+    @FXML
+    private AnchorPane AnchorPane;
+    @FXML
+    private TextArea CorrectText;
+    
+    @FXML
+    private void handleSpellCheck(ActionEvent event) {
+       
+       TextEditor.text= InputText.getText();
+       TextEditor.readWords();
+     
+       CorrectText.setText(TextEditor.spellChecker());
+        
+    }
+    
+    @FXML
+    private void handleSearchButton(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void handleOpen(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleSave(ActionEvent event) {
+        
+    }
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- oop1/src/oop1/TextEditor.java
+++ oop1/src/oop1/TextEditor.java
@@ -77,4 +77,7 @@ public class TextEditor {
         }
         return ret;
     }
+    
+    
+   
 }
