1459aec17c325e488c0c3c290bb8e4c526dea634
Berkhan <berkhanygt97@gmail.com>
2020-12-14 14:57:38 +0300

    ufak fxml önemli bişi yok

--- oop1/src/oop1/FXMLDocumentController.java
+++ oop1/src/oop1/FXMLDocumentController.java
@@ -7,26 +7,18 @@ package oop1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author berkh
  */
 public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
